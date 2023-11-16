b393ae528bdb9587941c3aac15326a28408752ce
maulsd667 <146180071+maulsd667@users.noreply.github.com>
2023-11-16 09:13:21 +0100

    cambios

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="headerBackground">
+            <p className="welcomemessage">Bienvenidos conejito sanchez</p>
+        </div>
+    );
+}
+
+export default Header;
