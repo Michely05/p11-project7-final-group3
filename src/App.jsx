11ada7c62fee883dece8b9febbfd356ccee05ea7
Matheus <matriblima97@gmail.com>
2023-11-16 09:40:34 +0100

    fixed problems w progresbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import ProgressBar from "./components/ProgressBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-  
+      <ProgressBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
