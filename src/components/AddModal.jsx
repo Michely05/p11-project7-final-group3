e4fb9fdfe0b91152252fa639ce8c6933d159d966
Matheus <matriblima97@gmail.com>
2023-11-20 11:29:14 +0100

    fetch post added to addModal component

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -12,8 +12,36 @@ function AddModal({ onAdd }) {
         setInputValue("");
     };
 
+    const fetchPost = async (data) => {
+        try {
+            const response = await fetch('TU_ENLACE_DE_API', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Puedes necesitar incluir encabezados adicionales aquí
+                },
+                body: JSON.stringify({ data }),
+            });
+
+            if (!response.ok) {
+                throw new Error('La respuesta de la red no fue correcta');
+            }
+
+            const responseData = await response.json();
+            console.log('Datos enviados correctamente:', responseData);
+        } catch (error) {
+            console.error('Error durante la llamada a fetchPost:', error);
+        }
+    };
+
     const handleAddClick = () => {
+        // Llama a la función fetchPost para enviar los datos al servidor
+        fetchPost(inputValue);
+
+        // Actualiza el estado local o realiza cualquier otra acción según sea necesario
         onAdd(inputValue);
+
+        // Reinicia el valor de la entrada
         setInputValue("Correr, Estudiar, Leer...");
     };
 
