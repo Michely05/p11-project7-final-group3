69cf5433ef1ac6fcbeebe9db364aa930ac4afaae
Matheus <matriblima97@gmail.com>
2023-11-24 14:04:14 +0100

    buttons fixed

--- src/components/ArrowButton/ArrowButton.jsx
+++ src/components/ArrowButton/ArrowButton.jsx
@@ -1,16 +1,12 @@
 import { Link } from "react-router-dom";
 import "./ArrowButton.css";
 import arrowbutton from "../../assets/img/backArrow.png";
-
-
 function ArrowButton() {
     return (
-        <div className="headerBackground">
+        <div className="arrowBtnContainer">
              <Link to={"/"}>
              <img src={arrowbutton} alt="Main Icon" className="icon" /></Link>
-            
         </div>
     );
 }
-
 export default ArrowButton;
