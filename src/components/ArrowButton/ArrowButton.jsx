e29677ed0a852c4b04d8e4cf3155c63109e1250d
maulsd667 <146180071+maulsd667@users.noreply.github.com>
2023-11-28 15:17:02 +0100

    readme.md

--- src/components/ArrowButton/ArrowButton.jsx
+++ src/components/ArrowButton/ArrowButton.jsx
@@ -4,11 +4,7 @@ import arrowbutton from "../../assets/img/backArrow.png";
 function ArrowButton() {
     return (
         <div className="arrowBtnContainer">
-<<<<<<< HEAD
-             <Link to={"/"}>
-=======
              <Link to={"/main"}>
->>>>>>> 973aafb49bdae13a85f447575f48504494d42d10
              <img src={arrowbutton} alt="Main Icon" className="icon" /></Link>
         </div>
     );
